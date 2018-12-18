b8a6b2a88c9f0f3550dd39ade7431df10be004b5
Cedric Djahan <cedric.djahan@capgemini.com>
2018-12-18 14:48:04 +0100

    Add method definition for script approval

--- src/com/capgemini/productionline/configuration/Jenkins.groovy
+++ src/com/capgemini/productionline/configuration/Jenkins.groovy
@@ -9,7 +9,7 @@ import org.jenkinsci.plugins.plaincredentials.impl.*
 import com.cloudbees.plugins.credentials.common.*
 import hudson.util.Secret
 import hudson.tools.*
-
+import org.jenkinsci.plugins.scriptsecurity.scripts.*
 
 import jenkins.model.*;
 import com.cloudbees.jenkins.plugins.customtools.CustomTool;
@@ -20,25 +20,25 @@ import com.synopsys.arc.jenkinsci.plugins.customtools.versions.ToolVersionConfig
 // import jenkins.model.*
 
 /**
- * Contains the configuration methods of the jenkins component
- * <p>
- *     The main purpose collecting configuration methods.
- *
- * Created by tlohmann on 19.10.2018.
- */
- class JenkinsConfiguration implements Serializable {
+* Contains the configuration methods of the jenkins component
+* <p>
+*     The main purpose collecting configuration methods.
+*
+* Created by tlohmann on 19.10.2018.
+*/
+class JenkinsConfiguration implements Serializable {
   /**
-   * Method for creating a global credential object in the jenkins context.
-   * <p>
-   * @param id
-   *    uniqe id for references in Jenkins
-   * @param desc
-   *    description for the credentials object.
-   * @param username
-   *    username of the credentials object.
-   * @param password
-   *    password of the credentials object.
-   */
+  * Method for creating a global credential object in the jenkins context.
+  * <p>
+  * @param id
+  *    uniqe id for references in Jenkins
+  * @param desc
+  *    description for the credentials object.
+  * @param username
+  *    username of the credentials object.
+  * @param password
+  *    password of the credentials object.
+  */
   public UsernamePasswordCredentialsImpl createCredatialObjectUsernamePassword(String id, String desc, String username, String password) {
     // create credential object
     def credObj = new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL, id, desc, username, password)
@@ -74,102 +74,129 @@ import com.synopsys.arc.jenkinsci.plugins.customtools.versions.ToolVersionConfig
   */
   public Boolean addCustomTool(String toolName, String commandLabel, String homeDir, String filePath, String exportedPaths, String toolHome, String additionalVariables){
 
-     def jenkinsExtensionList  = Jenkins.getInstance().getExtensionList(com.cloudbees.jenkins.plugins.customtools.CustomTool.DescriptorImpl.class)[0]
+    def jenkinsExtensionList  = Jenkins.getInstance().getExtensionList(com.cloudbees.jenkins.plugins.customtools.CustomTool.DescriptorImpl.class)[0]
 
-     def installs = jenkinsExtensionList.getInstallations()
-     def found = installs.find {
-       it.name == toolName
-     }
+    def installs = jenkinsExtensionList.getInstallations()
+    def found = installs.find {
+      it.name == toolName
+    }
 
-     if ( found ) {
-       println toolName + " is already installed. Nothing to do."
-       return false
-       } else {
-         println "installing " + toolName + " tool"
-
-         List installers = new ArrayList();
-
-         // read the file content from
-         File file = new File(filePath)
-
-          installers.add(new CommandInstaller(commandLabel, file.text, toolHome))
-          List<ToolProperty> properties = new ArrayList<ToolProperty>()
-          properties.add(new InstallSourceProperty(installers))
-
-         def newI = new CustomTool(toolName, homeDir, properties, exportedPaths, null, ToolVersionConfig.DEFAULT, additionalVariables)
-         installs += newI
-
-         jenkinsExtensionList.setInstallations( (com.cloudbees.jenkins.plugins.customtools.CustomTool[])installs );
-
-         jenkinsExtensionList.save()
-
-         return true;
-       }
-     }
-
-  /**
-   * Method for installing a jenkins plugin
-   * <p>
-   *    Installs the given list of Jenkins plugins if not installed already. Before installing a plugin, the UpdateCenter is updated.
-   * @param pluginsToinstall
-   *    list of plugins to install
-   * @return
-   *    Boolean value which reflects wether a plugin was installed and a restart is required
-   */
-  public Boolean installPlugin( pluginsToInstall ) {
-    def newPluginInstalled = false
-    def initialized = false
-    def instance = Jenkins.getInstance()
-
-    def pm = instance.getPluginManager()
-    def uc = instance.getUpdateCenter()
-
-    pluginsToInstall.each {
-      def String pluginName = it
-      // Check if the plugin is already installed.
-      if (!pm.getPlugin(pluginName)) {
-        println "Plugin not installed yet - Searching '$pluginName' in the update center."
-        // Check for updates.
-        if (!initialized) {
-          uc.updateAllSites()
-          initialized = true
-        }
-
-        def plugin = uc.getPlugin(pluginName)
-        if (plugin) {
-          println "Installing '$pluginName' Jenkins Plugin ..."
-          def installFuture = plugin.deploy()
-          while(!installFuture.isDone()) {
-            sleep(3000)
-          }
-          newPluginInstalled = true
-          println "... Plugin has been installed"
-        } else {
-          println "Could not find the '$pluginName' Jenkins Plugin."
-        }
+    if ( found ) {
+      println toolName + " is already installed. Nothing to do."
+      return false
       } else {
-        println "The '$pluginName' Jenkins Plugin is already installed."
+        println "installing " + toolName + " tool"
+
+        List installers = new ArrayList();
+
+        // read the file content from
+        File file = new File(filePath)
+
+        installers.add(new CommandInstaller(commandLabel, file.text, toolHome))
+        List<ToolProperty> properties = new ArrayList<ToolProperty>()
+        properties.add(new InstallSourceProperty(installers))
+
+        def newI = new CustomTool(toolName, homeDir, properties, exportedPaths, null, ToolVersionConfig.DEFAULT, additionalVariables)
+        installs += newI
+
+        jenkinsExtensionList.setInstallations( (com.cloudbees.jenkins.plugins.customtools.CustomTool[])installs );
+
+        jenkinsExtensionList.save()
+
+        return true;
       }
     }
-    return newPluginInstalled
-  }
 
-  /**
-   * Method for restarting jenkins
-   * <p>
-   *    perform a restart of the jenkins instance. This is necessary when for e.g. a new plugin is installed. The restart should allway be performed at the end of the configuration.
-   * @param safeRestart
-   *    Optional Boolean parameter stating if the restart should be safe (default: false)
-   */
-  public restartJenkins( safeRestart ) {
-    def instance = Jenkins.getInstance()
-    if ( safeRestart ) {
-      instance.safeRestart()
-    } else {
-      instance.restart()
+    /**
+    This method approves all scripts that are waiting for Approval
+    */
+    public approvePendingScript() {
+      ScriptApproval sa = ScriptApproval.get();
+
+      // approve scripts
+      for (ScriptApproval.PendingScript pending : sa.getPendingScripts()) {
+        sa.approveScript(pending.getHash());
+        println "Approved Script: " + pending.script
+      }
     }
-  }
-  public restartJenkins() {
-    restartJenkins( false )
-  }
-}
+
+    /**
+    This method approves all signature that are waiting for Approval
+    */
+    public approvePendingSignature() {
+      ScriptApproval sa = ScriptApproval.get();
+
+      // approve scripts
+      for (ScriptApproval.PendingSignature pending : sa.getPendingSignatures()) {
+        sa.approveSignature(pending.signature);
+        println "Approved Signature: " + pending.signature
+      }
+    }
+
+
+    /**
+    * Method for installing a jenkins plugin
+    * <p>
+    *    Installs the given list of Jenkins plugins if not installed already. Before installing a plugin, the UpdateCenter is updated.
+    * @param pluginsToinstall
+    *    list of plugins to install
+    * @return
+    *    Boolean value which reflects wether a plugin was installed and a restart is required
+    */
+    public Boolean installPlugin( pluginsToInstall ) {
+      def newPluginInstalled = false
+      def initialized = false
+      def instance = Jenkins.getInstance()
+
+      def pm = instance.getPluginManager()
+      def uc = instance.getUpdateCenter()
+
+      pluginsToInstall.each {
+        def String pluginName = it
+        // Check if the plugin is already installed.
+        if (!pm.getPlugin(pluginName)) {
+          println "Plugin not installed yet - Searching '$pluginName' in the update center."
+          // Check for updates.
+          if (!initialized) {
+            uc.updateAllSites()
+            initialized = true
+          }
+
+          def plugin = uc.getPlugin(pluginName)
+          if (plugin) {
+            println "Installing '$pluginName' Jenkins Plugin ..."
+            def installFuture = plugin.deploy()
+            while(!installFuture.isDone()) {
+              sleep(3000)
+            }
+            newPluginInstalled = true
+            println "... Plugin has been installed"
+            } else {
+              println "Could not find the '$pluginName' Jenkins Plugin."
+            }
+            } else {
+              println "The '$pluginName' Jenkins Plugin is already installed."
+            }
+          }
+          return newPluginInstalled
+        }
+
+        /**
+        * Method for restarting jenkins
+        * <p>
+        *    perform a restart of the jenkins instance. This is necessary when for e.g. a new plugin is installed. The restart should allway be performed at the end of the configuration.
+        * @param safeRestart
+        *    Optional Boolean parameter stating if the restart should be safe (default: false)
+        */
+        public restartJenkins( safeRestart ) {
+          def instance = Jenkins.getInstance()
+          if ( safeRestart ) {
+            instance.safeRestart()
+            } else {
+              instance.restart()
+            }
+          }
+          public restartJenkins() {
+            restartJenkins( false )
+          }
+        }
